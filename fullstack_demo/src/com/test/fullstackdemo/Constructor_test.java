16fb7c50599e1fd1b7d61bf3814278042aedeb6a
Bhargavi <bhargavi.seera@gmail.com>
2021-01-13 23:55:08 -0600

    modified

--- fullstack_demo/src/com/test/fullstackdemo/Constructor_test.java
+++ fullstack_demo/src/com/test/fullstackdemo/Constructor_test.java
@@ -2,16 +2,14 @@ package com.test.fullstackdemo;
 
 public class Constructor_test {
 	int x;
-	String name;
-	String noValue;
-
+	
 	 Constructor_test() {
 		
 		 System.out.println("This is no parameter constructor");
 		 
 	}
-	
-	public Constructor_test(int y, int z)
+	 
+	Constructor_test(int y, int z)
 	{
 		
 		x = y+z;
@@ -19,22 +17,17 @@ public class Constructor_test {
 		
 	}
 	
-	Constructor_test(String s) {
+	Constructor_test(String b) {
 		
-		System.out.println("parameterized constructor with string "+ s);
+		System.out.println("parameterized constructor with string "+ b);
 		
 	}
 	
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 				
 		Constructor_test con1 = new Constructor_test();
 		Constructor_test con2 = new Constructor_test(4,6);
 		Constructor_test con3 = new Constructor_test("Bhargavi");
-		
-		
-		
 
 	}
 }
